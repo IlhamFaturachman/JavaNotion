f983d7fab864b2a683d4581ad5fe2b1f05b90f10
IlhamFaturachman <69384419+IlhamFaturachman@users.noreply.github.com>
2021-07-19 12:46:16 +0700

    Updated

--- ObjectEzy/src/Array_ezy.java
+++ ObjectEzy/src/Array_ezy.java
@@ -5,8 +5,8 @@ public class Array_ezy {
         Scanner input = new Scanner(System.in);
         Scanner tanahInput = new Scanner(System.in);
         String [] Tanah = new String[3];
-        int [] Panjang = new int[3];
-        int [] Lebar = new int[3];
+        int [] Panjang = new int[Tanah.length];
+        int [] Lebar = new int[Tanah.length];
         int Luas;
         
         for (int i = 0; i < Tanah.length; i++){
@@ -21,8 +21,10 @@ public class Array_ezy {
             System.out.println(Tanah[i]);
             System.out.println("Panjang : " + Panjang[i]);
             System.out.println("Lebar : " + Lebar[i]);
+        }
+        for (int i = 0; i < Tanah.length; i++){
             Luas = Panjang[i] * Lebar[i];
-            System.out.println("Luas Tanah : " + Luas);
+            System.out.println("Luas " + Tanah[i] + " : " + Luas);
         }
     }
 }
